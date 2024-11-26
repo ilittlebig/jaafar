a57f1aa18134b3bb64d7807c3a5b9e39a05bbc0d
Elias Sjödin <elias@amsystem.com>
2024-11-26 19:39:21 +0100

    signing in now routes to the dashboard page

--- src/routes/(protected)/+layout.ts
+++ src/routes/(protected)/+layout.ts
@@ -1,10 +1,16 @@
 import { redirect } from "@sveltejs/kit";
+import { getCurrentSession } from "@ilittlebig/easy-auth";
 
 const isAuthenticated = async () => {
-  return false;
+	try {
+		await getCurrentSession();
+		return true;
+	} catch {
+		return false;
+	}
 }
 
 export const load = async () => {
   const authenticated = await isAuthenticated();
-	if (!authenticated) redirect(307, "/login");
+	if (!authenticated) throw redirect(307, "/login");
 }
