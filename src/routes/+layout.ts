72c818e3f3467485eea6e1f4173070c0751f2f68
Elias Sjödin <elias@amsystem.com>
2024-11-26 19:39:21 +0100

    signing in now routes to the dashboard page

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,5 @@
+import { goto } from "$app/navigation";
+import { Hub } from "@ilittlebig/easy-auth";
 import { configureEasyAuth } from "$lib/services/auth-service";
 import "../app.css";
 
@@ -5,3 +7,14 @@ export const prerender = true;
 export const ssr = false;
 
 configureEasyAuth();
+
+Hub.listen("auth", ({ payload }) => {
+	switch (payload.event) {
+		case "signedIn":
+			goto("/");
+			break;
+		case "signedOut":
+			goto("/login");
+			break;
+	}
+});
