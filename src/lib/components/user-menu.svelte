628153f1a2fee79492fe4d7ec1d825ec41128439
Elias Sjödin <elias@amsystem.com>
2024-11-27 21:47:24 +0100

    feat(dashboard): add header and user menu dropdown menu

---
+++ src/lib/components/user-menu.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+</script>
+
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger>
+		{#snippet child({ props })}
+			<Button
+				{...props}
+				variant="ghost"
+				size="icon"
+				class="rounded-lg "
+			>
+				<i class="fa-regular fa-circle-user text-lg"></i>
+				<span class="sr-only">Toggle user menu</span>
+			</Button>
+		{/snippet}
+  </DropdownMenu.Trigger>
+
+  <DropdownMenu.Content class="w-56" align="end" side="right">
+    <DropdownMenu.Label class="font-normal">
+      <div class="flex flex-col space-y-1">
+        <p class="text-sm font-medium leading-none">Elias</p>
+        <p class="text-muted-foreground text-xs leading-none">elias@example.com</p>
+      </div>
+    </DropdownMenu.Label>
+    <DropdownMenu.Separator />
+    <DropdownMenu.Item>Profile</DropdownMenu.Item>
+    <DropdownMenu.Item>
+      Settings
+      <DropdownMenu.Shortcut>⌘,</DropdownMenu.Shortcut>
+    </DropdownMenu.Item>
+    <DropdownMenu.Item>Billing</DropdownMenu.Item>
+    <DropdownMenu.Separator />
+    <DropdownMenu.Item>Sign Out</DropdownMenu.Item>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
