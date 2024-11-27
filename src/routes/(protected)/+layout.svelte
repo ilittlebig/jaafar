689681592dcd28be8567dfaddcb6e9637636f934
Elias Sjödin <elias@amsystem.com>
2024-11-27 21:47:24 +0100

    feat(dashboard): add header and user menu dropdown menu

--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
+	import Header from "$lib/components/header.svelte";
+	import UserMenu from "$lib/components/user-menu.svelte";
 	let { children } = $props();
 </script>
 
-LAYOUT
-
-{@render children()}
+<div class="grid min-h-screen w-full grid-cols-[85px_1fr]">
+  <div class="bg-background border-r h-full w-full">
+    <div class="flex h-full max-h-screen flex-col justify-between items-center pb-4">
+      <div class="flex w-full h-full flex-col items-center gap-4">
+				<div class="flex items-center border-b w-full h-[44px] px-6"></div>
+				Navbar
+      </div>
+			<UserMenu />
+    </div>
+  </div>
+  <div class="flex flex-col">
+		<Header />
+    <div class="flex flex-1 flex-col gap-6 p-6 bg-muted/40">
+			{@render children()}
+    </div>
+  </div>
+</div>
