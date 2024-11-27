8f2688c5a49d93f77f181cc0d41803fb3e7e58e6
Elias Sjödin <elias@amsystem.com>
2024-11-27 20:30:08 +0100

    feat(sign-up): add sign up functionality

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div class="flex items-center justify-center h-screen w-full relative">
+	{@render children()}
+	<div class="flex gap-x-8 absolute bottom-0 p-4">
+		<p class="text-sm text-muted-foreground">
+			Made with
+			<i class="fa-solid fa-heart text-destructive"></i>
+			by Jaafar.
+		</p>
+	</div>
+</div>
