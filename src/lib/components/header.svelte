714cf4e379d29383d1fc8b0501e33ca4cfb7d6d6
Elias Sjödin <elias@amsystem.com>
2024-11-29 23:18:56 +0100

    fix(header): would shrink occasionally depending on page content

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -2,7 +2,7 @@
 	import { Button } from "$lib/components/ui/button";
 </script>
 
-<header class="bg-background flex justify-end items-center gap-1 border-b h-[44px] px-2">
+<header class="bg-background flex justify-end items-center gap-1 border-b h-[44px] px-2 shrink-0">
   <Button
     variant="ghost"
     size="icon"
