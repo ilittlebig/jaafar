58d4df18c32f4a3d41cbfec4dd12c4226a06e552
Elias Sjödin <elias@amsystem.com>
2024-11-29 09:44:07 +0100

    feat(settings): working validation

--- src/lib/services/settings-service.svelte.ts
+++ src/lib/services/settings-service.svelte.ts
@@ -6,20 +6,35 @@
  */
 
 import { readFileJSON, writeFileJSON } from "$lib/services/files-service"
-import { settingsStore } from "$lib/stores/settings-store.svelte";
+import { settingsStore, type Settings } from "$lib/stores/settings-store.svelte";
 import { deepMerge } from "$lib/utils"
+
+const updateSettingsStore = (settings: Settings) => {
+	for (const key in settings) {
+		if (typeof settings[key] === "object" && settings[key] !== null) {
+			settingsStore[key] = {
+				...settingsStore[key],
+				...settings[key],
+			};
+		} else {
+			settingsStore[key] = settings[key];
+		}
+	}
+};
 
 export const loadSettings = async () => {
 	const settings = await readFileJSON("settings.json");
 	if (settings) {
-		const mergedSettings = deepMerge({ ...settingsStore }, settings);
-		for (const key in mergedSettings) {
-			settingsStore[key] = mergedSettings[key];
-		}
+		const mergedSettings = deepMerge(settingsStore, settings);
+		updateSettingsStore(mergedSettings);
 	}
 	await saveSettings();
 }
 
-export const saveSettings = async () => {
-	await writeFileJSON("settings.json", settingsStore);
+export const saveSettings = async (settings?: Settings) => {
+	if (settings) {
+		const updatedSettings = deepMerge(settingsStore, settings);
+		updateSettingsStore(updatedSettings);
+	}
+	await writeFileJSON("settings.json", settings || settingsStore);
 }
