df92d56f25f7dd2089fe900c63502d47b25fb52b
Elias Sjödin <elias@amsystem.com>
2024-12-01 14:52:02 +0100

    feat(import-accounts): add dialog for validation errors

--- src/lib/components/dialogs/import-accounts-dialog.svelte
+++ src/lib/components/dialogs/import-accounts-dialog.svelte
@@ -3,23 +3,43 @@
 </script>
 
 <script lang="ts">
-	import { open } from "@tauri-apps/plugin-dialog";
+	import { selectFile } from "$lib/services/files-service";
+	import { importAccounts } from "$lib/services/accounts-service";
 	import { Button, buttonVariants } from "$lib/components/ui/button";
 	import * as Dialog from "$lib/components/ui/dialog";
+  import * as Alert from "$lib/components/ui/alert";
+	import ImportValidationErrorsDialog from "$lib/components/dialogs/import-validation-errors-dialog.svelte";
 
-	const handleClick = async () => {
-		const file = await open({
+	let error: string | undefined = $state();
+	let validationErrors: Record<number, string[]> = $state([]);
+
+	const handleCsvImport = async () => {
+		error = undefined;
+		validationErrors = [];
+
+		const filePath = await selectFile({
 			multiple: false,
 			directory: false,
-			filters: [
-				{ name: "a", extensions: ["csv"] },
-			],
+			filters: [{ name: "extension", extensions: ["csv"] }],
 		});
-		console.log(file);
+		if (!filePath) return;
+
+		try {
+			validationErrors = await importAccounts(filePath);
+			if (Object.keys(validationErrors).length > 0) return;
+			importAccountsDialog.open = false;
+		} catch (err: any) {
+			error = err.message;
+		}
+	}
+
+	const onOpenChange = () => {
+		error = undefined;
+		validationErrors = [];
 	}
 </script>
 
-<Dialog.Root bind:open={importAccountsDialog.open}>
+<Dialog.Root bind:open={importAccountsDialog.open} {onOpenChange}>
 	<Dialog.Trigger class={buttonVariants({ variant: "default" })}>
 		<i class="fa-regular fa-file-import"></i>
 		Import
@@ -28,8 +48,28 @@
 		<Dialog.Header>
 			<Dialog.Title>Import Accounts</Dialog.Title>
 		</Dialog.Header>
+
+		{#if error}
+			<Alert.Root variant="destructive">
+				<i class="fa-regular fa-circle-exclamation text-lg absolute left-4 top-4"></i>
+				<Alert.Title>Error</Alert.Title>
+				<Alert.Description>{error}</Alert.Description>
+			</Alert.Root>
+		{/if}
+
+		{#if Object.keys(validationErrors).length > 0}
+			<Alert.Root variant="destructive">
+				<i class="fa-regular fa-circle-exclamation text-lg absolute left-4 top-4"></i>
+				<Alert.Title>Error</Alert.Title>
+				<Alert.Description class="flex gap-x-2 items-center">
+					The following issues were found in your CSV file:
+					<ImportValidationErrorsDialog {validationErrors} />
+				</Alert.Description>
+			</Alert.Root>
+		{/if}
+
 		<div class="flex gap-x-2">
-			<Button class="w-full h-48" variant="outline">
+			<Button class="w-full h-48" variant="outline" disabled>
 				<div class="flex flex-col h-full w-full text-left text-wrap">
 					<p class="text-base font-semibold">Text File</p>
 					<p class="font-normal text-muted-foreground">
@@ -37,7 +77,7 @@
 					</p>
 				</div>
 			</Button>
-			<Button onclick={handleClick} class="w-full h-48" variant="outline">
+			<Button onclick={handleCsvImport} class="w-full h-48" variant="outline">
 				<div class="flex flex-col justify-between h-full w-full text-left text-wrap">
 					<div class="flex flex-col">
 						<p class="text-base font-semibold">CSV File</p>
--- src/routes/(protected)/accounts/+page.svelte
+++ src/routes/(protected)/accounts/+page.svelte
@@ -2,25 +2,43 @@
 	import { Button } from "$lib/components/ui/button";
 	import { ScrollArea } from "$lib/components/ui/scroll-area";
 	import AccountsTable from "$lib/components/accounts-table.svelte";
-	import ImportAccountsDialog from "$lib/components/dialogs/import-accounts-dialog.svelte";
+	import ImportAccountsDialog, { importAccountsDialog } from "$lib/components/dialogs/import-accounts-dialog.svelte";
 </script>
 
 <div class="flex flex-col gap-y-2 w-full">
 	<div class="flex justify-between items-center">
 		<h1 class="font-semibold text-2xl">Accounts</h1>
 		<div class="flex gap-x-2 justify-end">
-			<Button variant="outline">
-				<i class="fa-regular fa-cog"></i>
-			</Button>
-			<Button variant="outline">
-				<i class="fa-regular fa-download"></i>
-				Export
-			</Button>
+			{#if false}
+				<Button variant="outline">
+					<i class="fa-regular fa-cog"></i>
+				</Button>
+				<Button variant="outline">
+					<i class="fa-regular fa-download"></i>
+					Export
+				</Button>
+			{/if}
 			<ImportAccountsDialog />
 		</div>
 	</div>
 </div>
 
-<ScrollArea orientation="horizontal" class="w-full">
-	<AccountsTable />
-</ScrollArea>
+{#if false}
+	<ScrollArea orientation="horizontal" class="w-full">
+		<AccountsTable />
+	</ScrollArea>
+{:else}
+	<div class="flex flex-col gap-y-4 items-center justify-center h-full">
+		<img src="/images/add_document.svg" width={200} height={200} alt="Import Accounts Illustration" />
+		<div class="flex flex-col gap-y-1 items-center">
+			<p class="font-semibold">No Accounts Found</p>
+			<p class="max-w-[350px] text-center text-sm text-muted-foreground">
+				You do not have any accounts yet. Click the import button to add accounts.
+			</p>
+		</div>
+		<Button onclick={() => importAccountsDialog.open = true}>
+			<i class="fa-regular fa-file-import"></i>
+			Import Accounts
+		</Button>
+	</div>
+{/if}
