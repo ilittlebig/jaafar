1c895558b903345c36f107663585ac8a853e5c48
Elias Sjödin <elias@amsystem.com>
2024-12-02 19:31:44 +0100

    feat(row-actions): add simple dropdown menus for row actions, no functionality

--- src/routes/(protected)/proxies/columns.ts
+++ src/routes/(protected)/proxies/columns.ts
@@ -5,8 +5,21 @@
  * Created: 2024-12-01
  */
 
+import { createRawSnippet, mount, unmount } from "svelte";
 import type { ProxyGroup } from "$lib/stores/proxies-store.svelte";
 import type { ColumnDef } from "@tanstack/table-core";
+import { renderSnippet } from "$lib/components/ui/data-table";
+import ProxiesRowActions from "$lib/components/row-actions/proxies-row-actions.svelte";
+
+const rowActionsCellSnippet = createRawSnippet(() => {
+	return {
+		render: () => `<div class="flex justify-end"></div>`,
+		setup: (target) => {
+			const comp = mount(ProxiesRowActions, { target });
+			return () => unmount(comp);
+		}
+	};
+});
 
 export const columns: ColumnDef<ProxyGroup>[] = [
   {
@@ -16,5 +29,12 @@ export const columns: ColumnDef<ProxyGroup>[] = [
   {
     accessorFn: (row: ProxyGroup) => row.originalFilePath,
     header: "Original File Path",
+  },
+  {
+		id: "actions",
+    header: "",
+		cell: ({ row }) => {
+      return renderSnippet(rowActionsCellSnippet, row.original);
+    },
   },
 ];
