0bf06d08a1dca6441eba30e609e547b96e1555d6
Elias Sjödin <elias@amsystem.com>
2024-12-02 21:43:52 +0100

    feat(delete-dialog): proxies row actions opens delete dialog, no functionality

---
+++ src/lib/components/dialogs/delete-dialog.svelte
@@ -0,0 +1,35 @@
+<script module>
+	export let deleteDialog = $state({ open: false });
+</script>
+
+<script lang="ts">
+	import { buttonVariants } from "$lib/components/ui/button";
+	import * as AlertDialog from "$lib/components/ui/alert-dialog";
+
+	interface Props {
+		title: string;
+		description: string;
+		actionLabel: string;
+	}
+
+	let {
+		title,
+		description,
+		actionLabel,
+	}: Props = $props();
+</script>
+
+<AlertDialog.Root bind:open={deleteDialog.open}>
+	<AlertDialog.Content class="max-w-[425px]">
+		<AlertDialog.Header>
+			<AlertDialog.Title>{title}</AlertDialog.Title>
+			<AlertDialog.Description>{description}</AlertDialog.Description>
+		</AlertDialog.Header>
+		<AlertDialog.Footer>
+			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+				<AlertDialog.Action class={buttonVariants({ variant: "destructive" })}>
+					{actionLabel}
+				</AlertDialog.Action>
+		</AlertDialog.Footer>
+	</AlertDialog.Content>
+</AlertDialog.Root>
--- src/routes/(protected)/proxies/+page.svelte
+++ src/routes/(protected)/proxies/+page.svelte
@@ -3,15 +3,22 @@
 	import { Button } from "$lib/components/ui/button";
 	import { ScrollArea } from "$lib/components/ui/scroll-area";
 	import { DataTable } from "$lib/components/data-table";
-	import NewProxGroupDialog, { newProxyGroupDialog } from "$lib/components/dialogs/new-proxy-group-dialog.svelte";
+	import NewProxyGroupDialog, { newProxyGroupDialog } from "$lib/components/dialogs/new-proxy-group-dialog.svelte";
+	import DeleteDialog from "$lib/components/dialogs/delete-dialog.svelte";
 	import { columns } from "./columns";
 </script>
+
+<DeleteDialog
+	title="Delete Proxy Group"
+	description="Are you sure you want to delete this proxy group? This action is irreversible."
+	actionLabel="Delete Group"
+/>
 
 <div class="flex flex-col gap-y-2 w-full">
 	<div class="flex justify-between">
 		<h1 class="font-semibold text-2xl">Proxies</h1>
 		<div class="flex gap-x-2 justify-end">
-			<NewProxGroupDialog />
+			<NewProxyGroupDialog />
 		</div>
 	</div>
 </div>
