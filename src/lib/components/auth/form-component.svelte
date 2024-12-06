417f6a8a34999fd8b469210bcf115dd646eecea9
Elias Sjödin <elias@amsystem.com>
2024-12-06 22:55:45 +0100

    Revert "Merge pull request #21 from ilittlebig/feature/phone-numbers"
    This reverts commit efb55b16aa761de805e05f5031635c1e52f6bd18, reversing
    changes made to f3bf27975dfa9f2b8ba50ea6d152c18e6d5fb5b8.

--- src/lib/components/auth/form-component.svelte
+++ src/lib/components/auth/form-component.svelte
@@ -8,38 +8,15 @@
 		type Infer,
 	} from "sveltekit-superforms";
 	import { zodClient } from "sveltekit-superforms/adapters";
-	import { selectFile } from "$lib/services/files-service";
 	import { Input } from "$lib/components/ui/input";
-	import { Button } from "$lib/components/ui/button";
 	import * as Form from "$lib/components/ui/form";
-	import * as Select from "$lib/components/ui/select";
-
-	// Helper function for determining of a select value is a group
-	function isValueGroup(value: ValueObj | ValueGroup): value is ValueGroup {
-		return (value as ValueGroup).group !== undefined;
-	}
-
-	type ValueObj = {
-		value: string;
-		label: string;
-		[key: string]: any;
-	}
-
-	type ValueGroup = {
-		group: string;
-		options: ValueObj[];
-	};
 
 	interface Field {
 		name: string;
 		label: string;
 		placeholder?: string;
-		description?: string;
-		extensions?: string[];
 		type?: string;
 		component?: Component;
-		values?: (ValueObj | ValueGroup)[];
-		allowDeselect?: boolean;
 	}
 
 	interface Props {
@@ -47,7 +24,7 @@
 		onsuccess?: () => void;
 		fields: Field[];
 		schema: z.ZodTypeAny;
-		data: Record<string, string | undefined>;
+		data: Record<string, string>;
 		children: Snippet<[boolean]>;
 	}
 
@@ -59,16 +36,6 @@
 		data,
 		children,
 	}: Props = $props();
-
-	const handleSelectFile = async (field: Field) => {
-		const filePath = await selectFile({
-			multiple: false,
-			directory: false,
-			filters: field.extensions ? [{ name: "extension", extensions: field.extensions }] : [],
-		});
-		if (!filePath) return;
-		$formData[field.name] = filePath;
-	}
 
   const handleValidForm = async (form: SuperValidated<Infer<typeof schema>>) => {
     const formData = form.data;
@@ -107,48 +74,14 @@
 						<Form.Label>{field.label}</Form.Label>
 					{/if}
 
-					{#if field.type === "file"}
-						<Button variant="outline" class="w-full" onclick={() => handleSelectFile(field)}>
-							{$formData[field.name] || "Select File"}
-						</Button>
-					{:else if field.type === "select" && field.values}
-						<Select.Root type="single" bind:value={$formData[field.name]} allowDeselect={field.allowDeselect}>
-							<Select.Trigger {...props}>
-								{field.values
-									.flatMap(obj => isValueGroup(obj) ? obj.options : [obj])
-									.find((obj: ValueObj) => obj.value === $formData[field.name])?.label ||
-									field.placeholder || "Select"}
-							</Select.Trigger>
-							<Select.Content>
-								{#each field.values as obj}
-									{#if isValueGroup(obj)}
-										<Select.Group>
-											<Select.GroupHeading>{obj.group}</Select.GroupHeading>
-											{#each obj.options as option}
-												<Select.Item value={option.value} class="pl-12">
-													{option.label}
-												</Select.Item>
-											{/each}
-										</Select.Group>
-									{:else}
-										<Select.Item value={obj.value}>{obj.label}</Select.Item>
-									{/if}
-								{/each}
-							</Select.Content>
-						</Select.Root>
-					{:else}
-						<Input
-							{...props}
-							type={field.type}
-							placeholder={field.placeholder}
-							bind:value={$formData[field.name]}
-						/>
-					{/if}
+					<Input
+						{...props}
+						type={field.type}
+						placeholder={field.placeholder}
+						bind:value={$formData[field.name]}
+					/>
 				{/snippet}
 			</Form.Control>
-			{#if field.description}
-				<Form.Description>{field.description}</Form.Description>
-			{/if}
 			<Form.FieldErrors />
 		</Form.Field>
 	{/each}
--- src/lib/components/data-table/data-table-toolbar.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	let { children } = $props();
-</script>
-
-<div class="flex items-center justify-between">
-	{@render children()}
-</div>
