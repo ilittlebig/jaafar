a0d012c44ea549b3a5388245605a75041bef09c2
Elias Sjödin <elias@amsystem.com>
2024-12-15 17:16:32 +0100

    feat: add user agent data spoofing

--- src-tauri/src/data/profiles.rs
+++ src-tauri/src/data/profiles.rs
@@ -1,5 +1,9 @@
 pub struct BrowserProfile {
     pub ua: &'static str,
+    pub ua_data_platform: &'static str,
+    pub architecture: &'static str,
+    pub bitness: &'static str,
+    pub platform_version: &'static str,
     pub webgl_vendor: &'static str,
     pub webgl_renderer: &'static str,
     pub platform: &'static str,
@@ -11,13 +15,17 @@ pub struct BrowserProfile {
 
 pub static PROFILES: &[BrowserProfile] = &[
     BrowserProfile {
-        ua: "abc",
-        webgl_vendor: "vendor",
-        webgl_renderer: "ehm",
-        platform: "wtf",
+        ua: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        ua_data_platform: "macOS",
+        architecture: "arm",
+        bitness: "64",
+        platform_version: "10.15.7",
+        webgl_vendor: "Google Inc. (Apple)",
+        webgl_renderer: "ANGLE (Apple, ANGLE Metal Renderer: Apple M1 Pro, Unspecified Version)",
+        platform: "MacIntel",
         language: "en-US",
         languages: &["en-US", "en"],
-        hardware_concurrency: 8,
+        hardware_concurrency: 10,
         device_memory: 8,
     },
 ];
--- src-tauri/src/utils/stealth.rs
+++ src-tauri/src/utils/stealth.rs
@@ -1,15 +1,19 @@
+use user_agent_parser::UserAgentParser;
+
 use crate::utils::plugins::generate_hide_plugins_script;
 use crate::data::profiles::BrowserProfile;
 
-///
+/// Generates a JavaScript stealth script to spoof browser properties.
 ///
 /// # Arguments
-/// -
+/// - `browser_profile`: Contains browser details to spoof.
 ///
 /// # Returns
-///
+/// - A string with the JavaScript stealth script.
 pub fn build_stealth_script(browser_profile: &BrowserProfile) -> String {
     let plugins_script = generate_hide_plugins_script();
+    let user_agent_data_script = generate_user_agent_data_string(browser_profile);
+
     let platform = browser_profile.platform;
     let webgl_vendor = browser_profile.webgl_vendor;
     let webgl_renderer = browser_profile.webgl_renderer;
@@ -24,6 +28,8 @@ pub fn build_stealth_script(browser_profile: &BrowserProfile) -> String {
         Object.defineProperty(navigator, 'webdriver', {{
             get: () => undefined,
         }});
+
+        {user_agent_data_script}
 
         const getParameter = WebGLRenderingContext.prototype.getParameter;
         WebGLRenderingContext.prototype.getParameter = function(parameter) {{
@@ -61,4 +67,101 @@ pub fn build_stealth_script(browser_profile: &BrowserProfile) -> String {
 
     let stealth_script = format!("{main_script}{worker_script}");
     stealth_script
+}
+
+fn generate_user_agent_data_string(browser_profile: &BrowserProfile) -> String {
+    let regexes_path = include_str!("../data/regexes.yaml");
+    let ua_parser = UserAgentParser::from_str(regexes_path).unwrap();
+    let product = ua_parser.parse_product(browser_profile.ua);
+
+    let browser_name: &str = product.name.as_deref().unwrap_or("Unknown");
+    let browser_version: &str = product.major.as_deref().unwrap_or("0");
+
+    let architecture = browser_profile.architecture;
+    let bitness = browser_profile.bitness;
+    let platform_version = browser_profile.platform_version;
+
+    let is_chromium_based = false;//matches!(browser_name, "Chrome" | "Google Chrome" | "Microsoft Edge" | "Chromium");
+    let brands = if is_chromium_based {
+        generate_brands(browser_name, browser_version)
+    } else {
+        "undefined".to_string()
+    };
+
+    let platform = if is_chromium_based {
+        format!("{}", browser_profile.ua_data_platform)
+    } else {
+        "undefined".to_string()
+    };
+
+    let is_mobile = if is_chromium_based {
+        "false".to_string()
+    } else {
+        "undefined".to_string()
+    };
+
+    let high_entropy_values = if is_chromium_based {
+        r#"
+            architecture: "{architecture}",
+            bitness: "{bitness}",
+            model: "",
+            platformVersion: "{platform_version}"
+        "#.to_string()
+    } else {
+        r#"
+            architecture: "NA",
+            bitness: "NA",
+            model: "NA",
+            platformVersion: "NA"
+        "#.to_string()
+    };
+
+    format!(r#"
+        Object.defineProperty(navigator, 'userAgentData', {{
+            get: () => ({{
+                brands: {brands},
+                platform: {platform},
+                mobile: {is_mobile},
+                getHighEntropyValues: keys => {{
+                    return new Promise(resolve => {{
+                        const highEntropyValues = {{
+                            {high_entropy_values}
+                        }};
+
+                        const result = keys.reduce((obj, key) => {{
+                            if (highEntropyValues[key] !== undefined) {{
+                                obj[key] = highEntropyValues[key];
+                            }}
+                            return obj;
+                        }}, {{}});
+                        resolve(result);
+                    }});
+                }}
+            }})
+        }});
+    "#)
+}
+
+fn generate_brands(browser_name: &str, browser_version: &str) -> String {
+    match browser_name {
+        "Chrome" | "Google Chrome" => format!(
+            r#"
+            [
+                {{ brand: "Google Chrome", version: "{browser_version}" }},
+                {{ brand: "Chromium", version: "{browser_version}" }},
+                {{ brand: "Not_A Brand", version: "24" }}
+            ]
+            "#
+        ),
+        "Microsoft Edge" => format!(
+            r#"
+            [
+                {{ brand: "Microsoft Edge", version: "{browser_version}" }},
+                {{ brand: "Chromium", version: "{browser_version}" }},
+                {{ brand: "Not_A Brand", version: "24" }}
+            ]
+            "#
+        ),
+        _ => panic!("Unsupported browser for brands generation: {}", browser_name),
+    }
 }
