09801c771e96f520779fd6897e1ed4661659ec51
Elias Sjödin <elias@amsystem.com>
2024-12-19 09:41:56 +0100

    feat: add realistic metrics to overview page

--- src/lib/components/dashboard/overview-page.svelte
+++ src/lib/components/dashboard/overview-page.svelte
@@ -6,40 +6,40 @@
 	<Card.Root>
 		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2" >
 			<Card.Title class="text-sm font-medium">Product Carting Success</Card.Title>
-			<i class="fa-regular fa-dollar-sign text-muted-foreground text-sm"></i>
+			<i class="fa-regular fa-shopping-cart text-muted-foreground text-sm"></i>
 		</Card.Header>
 		<Card.Content>
-			<div class="text-2xl font-bold">$45,231.89</div>
+			<div class="text-2xl font-bold">73%</div>
 			<p class="text-muted-foreground text-xs">+20.1% from last month</p>
 		</Card.Content>
 	</Card.Root>
 	<Card.Root>
 		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2" >
 			<Card.Title class="text-sm font-medium">Missed Opportunities</Card.Title>
-			<i class="fa-regular fa-dollar-sign text-muted-foreground text-sm"></i>
+			<i class="fa-regular fa-arrow-trend-down text-muted-foreground text-sm"></i>
 		</Card.Header>
 		<Card.Content>
-			<div class="text-2xl font-bold">$45,231.89</div>
+			<div class="text-2xl font-bold">6</div>
 			<p class="text-muted-foreground text-xs">+20.1% from last month</p>
 		</Card.Content>
 	</Card.Root>
 	<Card.Root>
 		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2" >
-			<Card.Title class="text-sm font-medium">Total Revenue</Card.Title>
-			<i class="fa-regular fa-dollar-sign text-muted-foreground text-sm"></i>
+			<Card.Title class="text-sm font-medium">Total Estimated Cart Value</Card.Title>
+			<i class="fa-regular fa-sack-dollar text-muted-foreground text-sm"></i>
 		</Card.Header>
 		<Card.Content>
-			<div class="text-2xl font-bold">$45,231.89</div>
+			<div class="text-2xl font-bold">$5,230.00</div>
 			<p class="text-muted-foreground text-xs">+20.1% from last month</p>
 		</Card.Content>
 	</Card.Root>
 	<Card.Root>
 		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2" >
-			<Card.Title class="text-sm font-medium">Total Revenue</Card.Title>
-			<i class="fa-regular fa-dollar-sign text-muted-foreground text-sm"></i>
+			<Card.Title class="text-sm font-medium">Cost per Successful Task</Card.Title>
+			<i class="fa-regular fa-tag text-muted-foreground text-sm"></i>
 		</Card.Header>
 		<Card.Content>
-			<div class="text-2xl font-bold">$45,231.89</div>
+			<div class="text-2xl font-bold">$0.25</div>
 			<p class="text-muted-foreground text-xs">+20.1% from last month</p>
 		</Card.Content>
 	</Card.Root>
