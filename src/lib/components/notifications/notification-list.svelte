45bf6db0a5443e59fe9ed61770ffa3171c2e79b0
Elias Sjödin <elias@amsystem.com>
2024-12-19 13:02:54 +0100

    refactor: split notification logic into components

---
+++ src/lib/components/notifications/notification-list.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Separator } from "$lib/components/ui/separator";
+	import NotificationRow from "$lib/components/notifications/notification-row.svelte";
+</script>
+
+<NotificationRow />
+<div class="-mx-4">
+	<Separator />
+</div>
+<NotificationRow />
+<div class="-mx-4">
+	<Separator />
+</div>
+<NotificationRow />
+<div class="-mx-4">
+	<Separator />
+</div>
+<NotificationRow />
+<div class="-mx-4">
+	<Separator />
+</div>
+<NotificationRow />
+<div class="-mx-4">
+	<Separator />
+</div>
+<NotificationRow />
+<div class="-mx-4">
+	<Separator />
+</div>
+<NotificationRow />
