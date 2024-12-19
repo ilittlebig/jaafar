858481fbee28a4a4e4f08a3591cb44f584fe81fa
Elias Sjödin <elias@amsystem.com>
2024-12-19 12:22:50 +0100

    feat: add notification popover

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,14 +1,61 @@
 <script lang="ts">
+	import { cn } from "$lib/utils";
 	import { Button } from "$lib/components/ui/button";
+	import { Separator } from "$lib/components/ui/separator";
+  import * as Popover from "$lib/components/ui/popover";
+
+	let open = $state(false);
 </script>
 
 <header class="bg-background flex justify-end items-center gap-1 border-b h-[44px] px-2 shrink-0">
-  <Button
-    variant="ghost"
-    size="icon"
-    class="rounded-lg z-20"
-    aria-label="Notifications"
-  >
-		<i class="fa-solid fa-bell text-lg"></i>
-  </Button>
+	<Popover.Root bind:open>
+		<Popover.Trigger>
+			{#snippet child({ props })}
+				<Button
+					variant="ghost"
+					size="icon"
+					class="rounded-lg z-20"
+					aria-label="Notifications"
+					{...props}
+				>
+					<i class={cn(open ? "fa-solid" : "fa-regular", "fa-bell text-lg")}></i>
+				</Button>
+			{/snippet}
+		</Popover.Trigger>
+		<Popover.Content class="w-80" align="end">
+			<div class="flex flex-col gap-y-4">
+				<h4 class="font-medium leading-none">Notifications</h4>
+				<div class="-mx-4">
+					<Separator />
+				</div>
+				<div class="flex flex-col">
+					<div class="flex gap-x-2 items-center">
+						<p class="text-sm">Jaafar</p>
+						<p class="text-xs text-muted-foreground">Today @ 10:15am</p>
+					</div>
+					<p class="text-sm text-muted-foreground">A new update is available.</p>
+				</div>
+				<div class="-mx-4">
+					<Separator />
+				</div>
+				<div class="flex flex-col">
+					<div class="flex gap-x-2 items-center">
+						<p class="text-sm">Jaafar</p>
+						<p class="text-xs text-muted-foreground">Today @ 10:15am</p>
+					</div>
+					<p class="text-sm text-muted-foreground">A new update is available.</p>
+				</div>
+				<div class="-mx-4">
+					<Separator />
+				</div>
+				<div class="flex flex-col">
+					<div class="flex gap-x-2 items-center">
+						<p class="text-sm">Jaafar</p>
+						<p class="text-xs text-muted-foreground">Today @ 10:15am</p>
+					</div>
+					<p class="text-sm text-muted-foreground">A new update is available.</p>
+				</div>
+			</div>
+		</Popover.Content>
+	</Popover.Root>
 </header>
