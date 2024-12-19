4356cda04fdf46636c1ceec5826b3f94e30f82e6
Elias Sjödin <elias@amsystem.com>
2024-12-19 12:49:55 +0100

    feat: add mark as read and remove notification buttons

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -37,9 +37,35 @@
 					<Separator />
 				</div>
 				<div class="flex flex-col">
-					<div class="flex gap-x-2 items-center">
-						<p class="text-sm">Jaafar</p>
-						<p class="text-xs text-muted-foreground">Today @ 10:15am</p>
+					<div class="flex justify-between items-center">
+						<div class="flex gap-x-2 items-center">
+							<p class="text-sm">Jaafar</p>
+							<p class="text-xs text-muted-foreground">Today @ 10:15am</p>
+						</div>
+						<div class="flex gap-x-2">
+							<Tooltip.Provider disableHoverableContent={true}>
+								<Tooltip.Root>
+									<Tooltip.Trigger>
+										<button aria-label="Mark as read" class="text-xs text-muted-foreground hover:text-foreground">
+											<i class="fa-regular fa-eye"></i>
+										</button>
+									</Tooltip.Trigger>
+									<Tooltip.Content side="bottom" align="end">
+										Mark as read
+									</Tooltip.Content>
+								</Tooltip.Root>
+								<Tooltip.Root>
+									<Tooltip.Trigger>
+										<button aria-label="Remove notification" class="text-xs text-muted-foreground hover:text-foreground">
+											<i class="fa-regular fa-trash-can"></i>
+										</button>
+									</Tooltip.Trigger>
+									<Tooltip.Content side="bottom" align="end">
+										Remove notification
+									</Tooltip.Content>
+								</Tooltip.Root>
+							</Tooltip.Provider>
+						</div>
 					</div>
 					<p class="text-sm text-muted-foreground">A new update is available.</p>
 				</div>
