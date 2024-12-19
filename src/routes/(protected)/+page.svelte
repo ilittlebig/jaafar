1affbe900937a18b199af752bee3c75d9f6d2519
Elias Sjödin <elias@amsystem.com>
2024-12-19 07:22:43 +0100

    feat: add placeholder cards, with placeholder metrics

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -1,3 +1,50 @@
+<script lang="ts">
+	import * as Card from "$lib/components/ui/card";
+</script>
+
 <div class="flex items-center">
   <h1 class="font-semibold text-2xl">Dashboard</h1>
+</div>
+
+<div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+	<Card.Root>
+		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2" >
+			<Card.Title class="text-sm font-medium">Total Revenue</Card.Title>
+			<i class="fa-regular fa-dollar-sign text-muted-foreground text-sm"></i>
+		</Card.Header>
+		<Card.Content>
+			<div class="text-2xl font-bold">$45,231.89</div>
+			<p class="text-muted-foreground text-xs">+20.1% from last month</p>
+		</Card.Content>
+	</Card.Root>
+	<Card.Root>
+		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2" >
+			<Card.Title class="text-sm font-medium">Total Revenue</Card.Title>
+			<i class="fa-regular fa-dollar-sign text-muted-foreground text-sm"></i>
+		</Card.Header>
+		<Card.Content>
+			<div class="text-2xl font-bold">$45,231.89</div>
+			<p class="text-muted-foreground text-xs">+20.1% from last month</p>
+		</Card.Content>
+	</Card.Root>
+	<Card.Root>
+		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2" >
+			<Card.Title class="text-sm font-medium">Total Revenue</Card.Title>
+			<i class="fa-regular fa-dollar-sign text-muted-foreground text-sm"></i>
+		</Card.Header>
+		<Card.Content>
+			<div class="text-2xl font-bold">$45,231.89</div>
+			<p class="text-muted-foreground text-xs">+20.1% from last month</p>
+		</Card.Content>
+	</Card.Root>
+	<Card.Root>
+		<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2" >
+			<Card.Title class="text-sm font-medium">Total Revenue</Card.Title>
+			<i class="fa-regular fa-dollar-sign text-muted-foreground text-sm"></i>
+		</Card.Header>
+		<Card.Content>
+			<div class="text-2xl font-bold">$45,231.89</div>
+			<p class="text-muted-foreground text-xs">+20.1% from last month</p>
+		</Card.Content>
+	</Card.Root>
 </div>
